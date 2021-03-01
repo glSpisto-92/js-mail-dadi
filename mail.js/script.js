db7320fe9581b03276986fa51e2f35ce7716e352
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-03-01 15:06:28 +0100

    struttura esercizio mail creata. da sistemare

--- mail.js/script.js
+++ mail.js/script.js
@@ -5,3 +5,12 @@ stampa un messaggio appropriato sull’esito del controllo.*/
 // 1. Chiedo all'utente la sua mail
 var mail = prompt('Inserisci la tua mail');
 
+//lista delle mail autorizzate
+var listaMail = ['dargen@92.com','boolean@careers.it','web@developer.it','spruoccolo@live.com'];
+
+//controllo di accesso alla lista
+if ( mail ? listaMail ){
+    alert('Ok, sei nel club privè Dev Boolean');
+} else{
+    alert('Sei un impostore! Vattene o ti denunzio!');
+}
