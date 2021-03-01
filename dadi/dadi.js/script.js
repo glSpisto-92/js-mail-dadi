8f03e4c295da0b191cc315ea789a98715bfa5f70
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-03-01 17:44:11 +0100

    aggiunto gioco dei dadi

--- dadi/dadi.js/script.js
+++ dadi/dadi.js/script.js
@@ -1,1 +1,15 @@
-alert('oooo')
+/*Gioco dei dadi
+Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+var numRandom = Math.floor(Math.random() * 6 + 1);
+var numRandomPc = Math.floor(Math.random() * 6 + 1);
+
+console.log('Il numero estratto per te è il ' + numRandom);
+console.log('Il numero estratto per il pc è il ' + numRandomPc);
+
+if( numRandom > numRandomPc ){
+    alert('Bravo, hai vinto contro il pc!');
+} else if ( numRandomPc > numRandom){
+    alert('mi spiace, stavolta ha vinto il pc!');
+}
