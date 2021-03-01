76a8281729589d487e3f5c6f22c60432855b77c9
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-03-01 14:36:42 +0100

    Aggiunti file progetti dadi e mail

---
+++ mail.js/script.js
@@ -0,0 +1,7 @@
+/*Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.*/
+
+// 1. Chiedo all'utente la sua mail
+var mail = prompt('Inserisci la tua mail');
+
